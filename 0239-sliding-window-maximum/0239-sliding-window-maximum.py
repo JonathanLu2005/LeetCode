4aa30ccc7aacc4e71e0cf93002cc28a39d849b40
JonathanLu2005 <95429781+JonathanLu2005@users.noreply.github.com>
2025-03-17 09:45:48 +0000

    Time: 185 ms (41.68%), Space: 31.2 MB (71.15%) - LeetHub

---
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.py
@@ -0,0 +1,67 @@
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        # idea is deleting elements that wont be max within window
+        # use a deque, always decreasing
+        # moment a value is greater than rest of deque
+        # will pop rest
+        # will add leftmost value to output
+        # then shift window
+        # basically to avoid redoing work, will just delete everything
+        # and keep max
+        # this is a monotic decreasing queue
+        # use queue to add and remove elements in o(1)
+        # only remove smaller elemnts if exist
+        # keep it in decreasing order
+        # pop when its no longer within bounds
+        # pop right most to keep it in decreasing monotonic
+
+        output = []
+        l = r = 0
+
+        q = collections.deque()
+
+        while r < len(nums):
+            # remove to keep decreasing monotonic
+            while q and nums[q[-1]] < nums[r]:
+                q.pop()
+            q.append(r)
+
+            # remove left if met window size
+            # hence why we store indexes
+            if l > q[0]:
+                q.popleft()
+            
+            # if in window size, add max val
+            # increment l when we reach k
+            if (r+1) >= k:
+                output.append(nums[q[0]])
+                l += 1
+            r += 1
+
+        return output
+
+        
+        """
+        
+        # have nums and window k
+        # then we get max of each widnow and return it
+
+        # we just need to check the last / next element tbf and see if thats greater
+        # only issue is when we reach outside of k and need to replace it
+        # then we need to find next biggest
+
+        # could pop k times get biggest
+        # then pop next see whose bigger
+        # then so on
+        window = nums[0:k]
+        results = []
+
+        results.append(max(window))
+
+        for i in range(k, len(nums)):
+            window.pop(0)
+            window.append(nums[i])
+            results.append(max(window))
+
+        return results
+        """
