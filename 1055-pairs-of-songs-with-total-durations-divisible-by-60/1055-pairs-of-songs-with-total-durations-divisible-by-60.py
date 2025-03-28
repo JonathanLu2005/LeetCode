9d8b0dcc99949cf8c98b7984e2ccddcb1a30ca2c
JonathanLu2005 <95429781+JonathanLu2005@users.noreply.github.com>
2025-03-28 09:06:44 +0000

    Time: 35 ms (16.58%), Space: 20.6 MB (98.93%) - LeetHub

---
+++ 1055-pairs-of-songs-with-total-durations-divisible-by-60/1055-pairs-of-songs-with-total-durations-divisible-by-60.py
@@ -0,0 +1,22 @@
+class Solution:
+    def numPairsDivisibleBy60(self, time: List[int]) -> int:
+        # we can store their remainder because thats all we care about!
+        # e.g 50 % 60 = 50, so we obviously want something with 10!
+        # or 110 and 70
+        # mod them by by 60, we get 50 and 10, add both and we get 60
+        for i in range(0, len(time)):
+            time[i] = time[i] % 60
+
+        # after this its 2 sum 
+        hashmap = {}
+        result = 0
+        time = time[::-1]
+
+        for x in time:
+            if 60 - x in hashmap:
+                result += hashmap[60-x]
+            if x == 0:
+                result += hashmap.get(x,0)
+            hashmap[x] = hashmap.get(x,0) + 1
+        return result
+
