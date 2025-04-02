8aa30255da17384442784458e78d5d61b20d3dba
JonathanLu2005 <95429781+JonathanLu2005@users.noreply.github.com>
2025-04-02 21:59:48 +0100

    Time: 542 ms (95.82%), Space: 0B (100%) - LeetHub

---
+++ 2110-employees-with-missing-information/2110-employees-with-missing-information.sql
@@ -0,0 +1,17 @@
+# Write your MySQL query statement below
+
+# id name
+
+# id salary
+
+# return id where missing info
+
+select t.employee_id
+from
+(
+    select * from employees left join salaries using(employee_id)
+    union
+    select * from employees right join salaries using(employee_id)
+) as t
+where t.salary is null or t.name is null
+order by employee_id;
