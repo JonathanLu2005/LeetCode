c688f50a4db5caab4847fb782dcf8d805812a611
JonathanLu2005 <95429781+JonathanLu2005@users.noreply.github.com>
2025-04-02 10:56:14 +0100

    Time: 79 ms (56.68%), Space: 21.8 MB (62.94%) - LeetHub

--- 1388-greatest-sum-divisible-by-three/1388-greatest-sum-divisible-by-three.py
+++ 1388-greatest-sum-divisible-by-three/1388-greatest-sum-divisible-by-three.py
@@ -9,6 +9,15 @@ class Solution:
 
         dp = [0,0,0]
         for num in nums:
+            # need copy so we can get all numbers
             for i in dp[:]:
                 dp[(i+num)%3] = max(dp[(i+num)%3], i + num)
         return dp[0]
+
+        # idea is that for each number we apply to each
+        # in index 0 1 2
+        # see if its better and we store
+        # in case we wanna use later
+        # i think dp like these need u to find a human way
+        # to solve it efficiently
+        # then write it in code
