e5421cd712c42978dd05c8946062c039fa35c59f
JonathanLu2005 <95429781+JonathanLu2005@users.noreply.github.com>
2025-04-06 10:43:42 +0100

    Time: 3 ms (46.81%), Space: 10.8 MB (16.39%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        string s = to_string(x);
+
+        string rs = s;
+        reverse(rs.begin(), rs.end());
+
+        return (s == rs);
+    }
+};
