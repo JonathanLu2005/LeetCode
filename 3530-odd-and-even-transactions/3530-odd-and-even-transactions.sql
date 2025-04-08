5def8da63da56dbd56b34b9da7e74b65a3c49aa7
JonathanLu2005 <95429781+JonathanLu2005@users.noreply.github.com>
2025-04-08 11:49:40 +0100

    Time: 289 ms (89.14%), Space: 0B (100%) - LeetHub

---
+++ 3530-odd-and-even-transactions/3530-odd-and-even-transactions.sql
@@ -0,0 +1,14 @@
+# Write your MySQL query statement below
+
+# transaction, amount, date
+
+# find sum amount of odd and even transaction for each day
+# otherwise 0
+# transation date asc
+
+select transaction_date,
+    sum(case when amount % 2 = 1 then amount else 0 end) as odd_sum,
+    sum(case when amount % 2 = 0 then amount else 0 end) as even_sum
+from transactions
+group by transaction_date
+order by transaction_date;
