11d7d2e99cf5168e8221370ba2a4444559be96a3
JonathanLu2005 <95429781+JonathanLu2005@users.noreply.github.com>
2025-04-12 09:08:35 +0100

    Time: 227 ms (86.41%), Space: 41.7 MB (85.12%) - LeetHub

---
+++ 1492-time-needed-to-inform-all-employees/1492-time-needed-to-inform-all-employees.py
@@ -0,0 +1,36 @@
+class Solution:
+    def numOfMinutes(self, n: int, headID: int, manager: List[int], informTime: List[int]) -> int:
+        # n employees with id 0 to n-1
+        # head has head id
+        # employee has manager where amnager i is manager of employee
+        # tree structure
+        # i-th employee need inform time minute to inform suborindates
+        # return num minutes to inform employee
+        # hashmap, manager = subordinates
+        # then bfs and apply inform time 
+        # only if they've subordinates
+
+        if n <= 1:
+            return 0
+
+        hashmap = {}
+
+        for emp in range(0, n):
+            if emp != headID:
+                m = manager[emp]
+                if m not in hashmap:
+                    hashmap[m] = []
+                hashmap[m].append(emp)
+
+        queue = deque([(headID, 0)])
+        res = 0
+
+        while queue:
+            curNode, curTime = queue.popleft()
+            res = max(res, curTime)
+
+            if curNode in hashmap:
+                for sub in hashmap[curNode]:
+                    queue.append((sub, curTime + informTime[curNode]))
+
+        return res
