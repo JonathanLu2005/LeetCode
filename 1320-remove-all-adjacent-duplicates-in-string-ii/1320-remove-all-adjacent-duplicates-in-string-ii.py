0edb7bb82502f2bb9ce39e7328ec99b8cb92b72a
JonathanLu2005 <95429781+JonathanLu2005@users.noreply.github.com>
2025-04-19 10:04:03 +0100

    Time: 49 ms (38.24%), Space: 21.7 MB (79.14%) - LeetHub

--- 1320-remove-all-adjacent-duplicates-in-string-ii/1320-remove-all-adjacent-duplicates-in-string-ii.py
+++ 1320-remove-all-adjacent-duplicates-in-string-ii/1320-remove-all-adjacent-duplicates-in-string-ii.py
@@ -1,5 +1,21 @@
 class Solution:
     def removeDuplicates(self, s: str, k: int) -> str:
+        # keep count for every character append to as a tuple
+        # if hit k, remove it, else keep whatever and concatenate and return as result
+        stack = []
+
+        for x in s:
+            if stack and stack[-1][0] == x:
+                stack[-1] = (x, stack[-1][1] + 1)
+
+                if stack[-1][1] == k:
+                    stack.pop()
+            else:
+                stack.append((x,1))
+
+        return "".join(x * count for x, count in stack)
+
+        """ original
         # stack
         # keep appending, check if last k is same, if so remove
         # then check again in case, if chill then its chill we can continue on
@@ -13,3 +29,4 @@ class Solution:
                     stack = stack[:-k]
 
         return "".join(stack)
+        """
