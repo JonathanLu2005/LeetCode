ddad89feaec559fc16f4d859033754e906ec8c3c
JonathanLu2005 <95429781+JonathanLu2005@users.noreply.github.com>
2025-04-29 21:10:05 +0100

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3455-minimum-length-of-string-after-operations/3455-minimum-length-of-string-after-operations.py
+++ 3455-minimum-length-of-string-after-operations/3455-minimum-length-of-string-after-operations.py
@@ -4,6 +4,12 @@ class Solution:
         # and get to remove
         # if >= 3, then bound to happen
         # keep deleting till < 3
+
+        # get length e.g 3 and 4
+        # add 1 to make it even 4 4
+        # mod by 2 to get 2
+        # - 1
+        # then that the amount to subtract by
         r = 0
 
         hashmap = {}
@@ -12,8 +18,15 @@ class Solution:
             hashmap[x] = hashmap.get(x,0) + 1
 
         for key, value in hashmap.items():
-            while value > 2:
-                value -= 2
-            r += value
+            if value > 2:
+                if value % 2 == 1:
+                    v = value + 1
+                else:
+                    v = value
+                remainder = (v/2) - 1
+                v -= remainder * 2
+                r += v
+            else:
+                r += value
         
         return r
