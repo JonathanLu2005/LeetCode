ed99581046c165de4ece9af59b6f992a614eea2f
JonathanLu2005 <95429781+JonathanLu2005@users.noreply.github.com>
2025-05-06 21:09:48 +0100

    Time: 0 ms (100%), Space: 22.9 MB (89.88%) - LeetHub

---
+++ 0275-h-index-ii/0275-h-index-ii.py
@@ -0,0 +1,30 @@
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        # citation i = citation for ith paper
+        # h index = max value of h, reseacher published h times and cited h times
+        # length = n papers
+        # h  = x papers, cited x times
+        # opposite? 6 = 1, 5 = 2, 3 = 3
+        # so given between value 1-5
+        # index it, e.g 3 goes to 3, realise length is 3 so printed at leas
+        # and value is 3 so other must be at least 3 or more
+        
+        # h = h papers with h citations (at least)
+        # between 0 to n
+        # 
+        
+        n = len(citations)
+        l = 0
+        r = n - 1
+        res = 0
+
+        while l <= r:
+            m = (l+r) // 2
+
+            if citations[m] >= n - m:
+                res = n - m
+                r = m - 1
+            else:
+                l = m + 1
+
+        return res
