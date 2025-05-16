edec6e5f2236ff73e4e32a3445be6d8cb2e30a7e
JonathanLu2005 <95429781+JonathanLu2005@users.noreply.github.com>
2025-05-16 21:20:30 +0100

    Time: 77 ms (51.09%), Space: 68.2 MB (14.69%) - LeetHub

---
+++ 3434-find-the-number-of-distinct-colors-among-the-balls/3434-find-the-number-of-distinct-colors-among-the-balls.py
@@ -0,0 +1,46 @@
+class Solution:
+    def queryResults(self, limit: int, queries: List[List[int]]) -> List[int]:
+        # l + 1 balls, with labels 0 to l
+        # balls uncoloured
+        # query, mark x ball with y
+        # then return result where i denote num of colours after ith query
+        # e.g
+        # 0 0 0 0 0
+        #   4
+        #   4 5
+        #   3 5
+        #   3 5 4
+        # could go through each query, update, then see what colours are like
+        # n^2
+        # or keep hashmap of each pos = colour
+        # hashmap of colour = count
+        # set = colours so far
+        posMap = {}
+        colMap = {}
+        cur = set()
+
+        result = []
+
+        for [ball, colour] in queries:
+            if ball not in posMap:
+                posMap[ball] = colour
+            else:
+                # else already exist
+                prev = posMap[ball]
+                colMap[prev] -= 1
+
+                # no more
+                if colMap[prev] == 0:
+                    cur.remove(prev)
+                posMap[ball] = colour
+            #brand new colour so we add to current
+            if colour not in colMap or colMap[colour] == 0:
+                cur.add(colour)
+            colMap[colour] = colMap.get(colour,0) + 1
+
+            result.append(len(cur))
+        return result
+
+
+
+            
