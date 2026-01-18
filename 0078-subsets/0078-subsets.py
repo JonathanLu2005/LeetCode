ce9b997fa924f67b3668d0cddd527cb2169bf8c2
JonathanLu2005 <95429781+JonathanLu2005@users.noreply.github.com>
2026-01-18 18:28:24 +0000

    Time: 0 ms (100%), Space: 19.5 MB (13.9%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,59 +1,20 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        res = []
+        # keep track of index and choose to keep index or not
+        result = []
+        n = len(nums)
         subset = []
 
-        def dfs(i):
-            if i >= len(nums):
-                res.append(subset.copy())
-                return 
+        def backtrack(index):
+            if index == n:
+                result.append(subset[:])
+                return
 
-            subset.append(nums[i])
-            dfs(i+1)
-            subset.pop()
-            dfs(i+1)
+            subset.append(nums[index])
+            backtrack(index+1)
 
-        dfs(0)
-        return res
+            subset.pop(-1)
+            backtrack(index+1)
+        backtrack(0)
+        return result
 
-
-
-
-
-
-
-
-        """
-        # like decision tree, add 1 and not add 1
-        # then add 2 or not add 2, so on with 3
-        # then have subset and decide if want to add 3 or not,
-        # just repeat decision and be like want to add this or not
-        # like deciison tree with many different altneratives
-        # get all results
-
-        #result list
-        res = []
-
-        # array to hold reslt 
-        subset = []
-        # use dfs, i is index of value making decision on
-        def dfs(i):
-            # bc if out of bound
-            if i >= len(nums):
-                # add copy, then subset will be modified later
-                res.append(subset.copy())
-                return 
-
-            # decision to include
-            subset.append(nums[i])
-            # then make deicsion on next el
-            dfs(i + 1)
-
-            # decision not to
-            subset.pop()
-            dfs(i + 1)
-            # then keep going to see if want 
-
-        dfs(0)
-        return res
-        """
