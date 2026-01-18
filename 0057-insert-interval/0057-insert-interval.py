c14c9150d1351a3dc58db3c6729b149f37da5eba
JonathanLu2005 <95429781+JonathanLu2005@users.noreply.github.com>
2026-01-18 16:30:58 +0000

    Time: 12 ms (7.12%), Space: 21.4 MB (10.79%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.py
+++ 0057-insert-interval/0057-insert-interval.py
@@ -1,39 +1,21 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-        # basically we insert a new interval, and need merge it with toher intervals
-        # hold the new start and end,
-        # if interval doesnt intersect, then we chill and add it and continue
-        # if it does, we update new start and new end
-        # such that if next one need to merge, we merge it
-        # else we add it
-        # have a value to see if we added midway or need add at end
-        # if add at end then add it at end
-        if not intervals:
-            return [newInterval]
+        # we could just add it in, sort it, and merge but that'll be nlogn
+        # so go through everything see if it overlap if so merge, else add to start or end
+        intervals.append(newInterval)
+        intervals = sorted(intervals)
 
-        newStart, newEnd = newInterval[0], newInterval[1]
-        result = []
-        #start = False
-        added = False
+        n = len(intervals)
+        i = 0
 
-        #if newStart < intervals[0][0] and newEnd < intervals[0][0]:
-        #    intervals.insert(0,[newStart,newEnd])
-        #    return intervals
+        while i < n-1:
+            curX, curY = intervals[i][0], intervals[i][1]
+            nexX, nexY = intervals[i+1][0], intervals[i+1][1]
 
-        for [x,y] in intervals:
-            if newStart < x and newEnd < x:
-                if not added:
-                    result.append([newStart,newEnd])
-                    added = True
-                result.append([x,y])
-            elif x <= newEnd and newStart <= y:
-                #start = True
-                newStart = min(x,newStart)
-                newEnd = max(y,newEnd)
+            if nexX <= curY:
+                intervals[i] = [min(curX,nexX),max(curY,nexY)]
+                intervals.pop(i+1)
+                n -= 1
             else:
-                result.append([x,y])
-
-        if not added:
-            result.append([newStart, newEnd])
-
-        return result
+                i += 1
+        return intervals
