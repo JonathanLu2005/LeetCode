474e1c6b41ed8e554d29dff00f89b84348301c2f
JonathanLu2005 <95429781+JonathanLu2005@users.noreply.github.com>
2026-01-18 13:51:17 +0000

    Time: 13 ms (79.03%), Space: 19.5 MB (7.04%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,76 +1,25 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        unique = set()
-        window = []
-        res = 0
+        # find longest substring without any duplicate characters
+        track = set()
+        start = 0
+        result = 0
+        current = 0
+        n = len(s)
 
-        for x in s:
-            window.append(x)
-            unique.add(x)
+        for i in range(0,n):
+            letter = s[i]
 
-            if len(window) != len(unique):
-                res = max(res, len(unique))
+            if letter in track:
+                result = max(result,current)
 
-                while len(window) != len(unique):
-                    y = window.pop(0)
-                    unique.remove(y)
-                    unique.add(x)
-
-        res = max(res, len(unique))
-        return res
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        """
-        charSet = set()
-        l = 0
-        res = 0
-
-        for r in range(len(s)):
-            while s[r] in charSet:
-                charSet.remove(s[l])
-                l += 1
-            charSet.add(s[r])
-            # able solve fact that bcaa, need to get rid of prev, but still wanna
-            # continue on e.g baca, get rid of ba but not c achieve that removal
-            # then new resas we keep moving along and r is incrementing by 1 auto
-            # same idea of sliding window just need to program differnetly
-            # try breaking down further
-            res = max(res, r - l + 1)
-        return res
-        """
-
-        """
-        mySet = {}
-        left = 0
-        right = 1
-        mx = 0
-        c = 1
-        mySet.append(s[left])
-
-        while right < len(s):
-            if s[right] in mySet:
-                left += 1
-                mx = max(mx, c)
-                c -= 1
-                right += 1
-            else:
-                mySet.append(s[right])
-                right += 1
-                c += 1
-
-        return max(mx, c)
-        """
+                while letter in track:
+                    left = s[start]
+                    track.remove(left)
+                    current -= 1
+                    start += 1
+                
+            current += 1
+            track.add(letter)
+        result = max(result,current)
+        return result
