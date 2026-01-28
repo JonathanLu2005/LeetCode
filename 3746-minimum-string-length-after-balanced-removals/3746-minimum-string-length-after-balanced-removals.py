71fab1b296b16f3bd2f11332df0c7db4f178c3ec
JonathanLu2005 <95429781+JonathanLu2005@users.noreply.github.com>
2026-01-28 09:13:30 +0000

    Time: 27 ms (47.53%), Space: 19.9 MB (19.27%) - LeetHub

--- 3746-minimum-string-length-after-balanced-removals/3746-minimum-string-length-after-balanced-removals.py
+++ 3746-minimum-string-length-after-balanced-removals/3746-minimum-string-length-after-balanced-removals.py
@@ -5,13 +5,12 @@ class Solution:
         # just get length, count number of a and b, get minimum of that, and take it away from the length times 2?
         a = 0
         b = 0
-        n = 0
 
         for x in s:
             if x == "a":
                 a += 1
             else:
                 b += 1
-            n += 1
-        res = min(a,b)
-        return n - (2 * res)
+
+        # return abs of a - b ? since it'll get us the remainder we want
+        return abs(a-b)
