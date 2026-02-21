b03532f20bbd10ed03fccaa324816334d86e137f
JonathanLu2005 <95429781+JonathanLu2005@users.noreply.github.com>
2026-02-21 10:16:41 +0000

    Time: 280 ms (31.88%), Space: 21.4 MB (56.23%) - LeetHub

---
+++ 2182-construct-string-with-repeat-limit/2182-construct-string-with-repeat-limit.py
@@ -0,0 +1,66 @@
+class Solution:
+    def repeatLimitedString(self, s: str, repeatLimit: int) -> str:
+        # string s
+        # repeat limit
+        # new string using character from s
+        # such that no letter appear more than repeat limit in row
+        # return largest possible
+        # i think
+        # 1 => we need largest letter first
+        # 2 => alternate letter well to include everything
+        # e.g 9 b, 3 a, 3 c
+        # bbb aaa bbb ccc bbb
+        # ccc bbb aaa bbb
+        # length matters
+        # balance
+        # whats better zzzz zzzaaaaaaaaaaaa
+        # so legit the game plan is to focus on THE biggest
+
+        # hashmap => get every letter count
+        # heap(?) => sort by biggest letter, we add biggest letter, add 2nd biggest, then put biggest back in, and add whatever
+        # until cant anymore
+
+        # c b a, get c from heap add to result, then get b, then if we can add c back in so next time we get c else we get a
+
+        # if its the biggest => add all of it
+        # if its the second biggest => add 1 of it
+        count = {}
+
+        for x in s:
+            count[x] = count.get(x,0) + 1
+
+        result = ""
+
+        heap = []
+
+        for key in count.keys():
+            heap.append((-ord(key), key))
+
+        heapq.heapify(heap)
+
+        temporary = None
+        while heap:
+            order, letter = heapq.heappop(heap)
+
+            if temporary:
+                heapq.heappush(heap, temporary)
+
+            if not temporary or (temporary and order < temporary[0]):
+                add = min(count[letter],repeatLimit)
+            else:
+                add = 1
+
+            result += (letter * add)
+            count[letter] -= add
+
+            if count[letter] > 0:
+                temporary = (order, letter)
+            else:
+                temporary = None
+
+        return result
+
+
+
+
+        
