583256a4da0a98c1834642255510a88e0637b2e3
JonathanLu2005 <95429781+JonathanLu2005@users.noreply.github.com>
2026-03-11 10:55:11 +0000

    Time: 269 ms (20.71%), Space: 26.8 MB (21.32%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,38 +1,35 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        row = len(grid)
-        column = len(grid[0])
-        
-        visitedStates = set()
+        # 1 is land, adjacent is island
+        # so given a 1,we bfs
+        # once we covered all 1,thats an island, we icrnement count
+        # wealso need to store the coordinates somewhere
+        #to avoid re exploring an island
+
+        visited = set()
         result = 0
+        row = len(grid)
+        col = len(grid[0])
 
         def bfs(x,y):
             directions = [[0,1],[0,-1],[1,0],[-1,0]]
-            queue = [(x,y)]
+            queue = deque([(x,y)])
 
             while queue:
+                x, y = queue.popleft()
 
-                for i in range(len(queue)):
-                    coordinates = queue.pop(0)
-                    cx, cy = coordinates
+                for [dirx,diry] in directions:
+                    curx = x + dirx
+                    cury = y + diry
 
-                    for [dirx, diry] in directions:
-                        curx = cx + dirx
-                        cury = cy + diry
-
-                        if curx in range(0,row) and cury in range(0,column) and (curx,cury) not in visitedStates and grid[curx][cury] == "1":
-                            queue.append((curx,cury))
-                        visitedStates.add((curx,cury))
+                    if curx >= 0 and curx < row and cury >= 0 and cury < col and (curx,cury) not in visited and grid[curx][cury] == "1":
+                        queue.append((curx,cury))
+                        visited.add((curx,cury))
             return
-
-
-        for x in range(0,row):
-            for y in range(0,column):
-                if (x,y) in visitedStates:
-                    continue
-                elif grid[x][y] == "0":
-                    visitedStates.add((x,y))
-                else:
+            
+        for x in range(row):
+            for y in range(col):
+                if (x,y) not in visited and grid[x][y] == "1":
                     result += 1
                     bfs(x,y)
         return result
