b8b91f1fbc6a18d4c49bfebba6e77128292c47c5
JonathanLu2005 <95429781+JonathanLu2005@users.noreply.github.com>
2026-04-18 14:32:30 +0100

    Time: 0 ms (100%), Space: 19.3 MB (72.63%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,21 +1,20 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        # backtracking, have 2 choices, we either add, or we dont
-        # keep track of index and decide if we want to add or not add
-        result = []
-        n = len(nums)
+        # backtracking, 2 choices, keep or not keep, and continue on
 
-        def backtrack(index,subset):
-            if index == n:
+        result = []
+
+        def backtracking(index, subset):
+            if index == len(nums):
                 result.append(subset.copy())
                 return
 
             subset.append(nums[index])
             index += 1
-            backtrack(index, subset)
+            backtracking(index,subset)
 
             subset.pop(-1)
-            backtrack(index, subset)
+            backtracking(index,subset)
 
-        backtrack(0,[])
+        backtracking(0,[])
         return result
