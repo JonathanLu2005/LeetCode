6e9ba27c3e88b0b07e8fcc96fafa805d150886d1
JonathanLu2005 <95429781+JonathanLu2005@users.noreply.github.com>
2026-04-18 14:58:21 +0100

    Time: 7 ms (47.73%), Space: 20.3 MB (28.3%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -3,21 +3,24 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        # traverse everything
-        # hold rows and columns that need to become 0
-        rows = set()
-        columns = set()
+        # have set of all possible column and row
+        # traverse matrix, moment its 0, we turn th whole row and column 0
+        # HOWEVER, we store the column and row thats been set to 0
+        # so if its another column then its like yh we ignore
+        # actually, as we go along, when we find 0, we store the row and column
+        # so the moment we get to next coordinate, if its part of row and column we set 0
+        # mmm but idk we might miss some
+        # find where all row and column 0 are, then set it all 0?
+        row = set()
+        column = set()
 
-        for row in range(len(matrix)):
-            for column in range(len(matrix[0])):
-                if matrix[row][column] == 0:
-                    rows.add(row)
-                    columns.add(column)
+        for x in range(len(matrix)):
+            for y in range(len(matrix[0])):
+                if matrix[x][y] == 0:
+                    row.add(x)
+                    column.add(y)
 
-        for row in rows:
-            for column in range(len(matrix[0])):
-                matrix[row][column] = 0
-
-        for row in range(len(matrix)):
-            for column in columns:
-                matrix[row][column] = 0
+        for x in range(len(matrix)):
+            for y in range(len(matrix[0])):
+                if x in row or y in column:
+                    matrix[x][y] = 0
