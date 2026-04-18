93ff2f241dd8de6697e9d9ce7584c40e49050bf9
JonathanLu2005 <95429781+JonathanLu2005@users.noreply.github.com>
2026-04-18 12:00:23 +0100

    Time: 70 ms (37.83%), Space: 22.2 MB (54.5%) - LeetHub

--- 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.py
+++ 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.py
@@ -1,5 +1,15 @@
 class Solution:
     def numSubarrayProductLessThanK(self, nums: List[int], k: int) -> int:
+        # if we know 5 2 6 are all < 100, so 60
+        # that means 5 2 6, 5 2, 2 6, and 5 2 6 are valid
+        # which is the equivalent of 3 (3 + 1) / 2
+        # 10 5, 2 (2 + 1) / 2 = 3
+        # issue is that we're counting 5 again, so we do the above calculation but minus length
+        # to only considefr pair
+        # then as we go for each x we can increment result if they're < k
+        # and if it above, we keep dividing 
+        # instead can have variable to hold length and what to remove and current to remove the extra array
+        # 
         if k <= 1:
             return 0
 
