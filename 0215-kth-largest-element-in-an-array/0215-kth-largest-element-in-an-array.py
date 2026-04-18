9cb2eb2285024189be776c2c730a1c07577ecf83
JonathanLu2005 <95429781+JonathanLu2005@users.noreply.github.com>
2026-04-18 12:22:51 +0100

    Time: 69 ms (66.31%), Space: 31.2 MB (13.77%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
@@ -1,12 +1,22 @@
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        # largest, minheap, so make everything negative
-        # and return k
+        # with heap need to make value negative as doing it by frewquency want to do by how important the number is
+        hashmap = {}
 
-        heap = [-x for x in nums]
+        for x in nums:
+            hashmap[x] = hashmap.get(x,0) + 1
+
+        heap = []
+
+        for key, value in hashmap.items():
+            heap.append((-1 * key))
+
         heapq.heapify(heap)
 
         while k > 0:
-            result = heapq.heappop(heap)
-            k -= 1
-        return (result * -1)
+            key = heapq.heappop(heap)
+            key *= -1
+            result = key
+            k -= hashmap[key]
+
+        return result
