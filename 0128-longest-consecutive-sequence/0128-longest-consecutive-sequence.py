396725ffe4309712f29fd5b608be4a15f078d393
JonathanLu2005 <95429781+JonathanLu2005@users.noreply.github.com>
2026-04-18 18:54:21 +0100

    Time: 43 ms (85.1%), Space: 32.6 MB (95.61%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -1,31 +1,25 @@
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-        # ok so conseuctive is incremental by 1
-        # so we sort it, and we can skip elements too
-        # sort it, we traverse, if its incrementing by 1, we include it
-        # if its the same, we just ignore
-        # and moment we hit a number that isnt incrementing by 1,we store this result and reset coutner and continue
+        # if samewe can skip as we dont care
         if not nums:
             return 0
-
         nums = sorted(nums)
-        total = 0
-
         result = 1
-        previousX = nums[0]
+        current = 1
+        prev = nums[0]
 
         for i in range(1,len(nums)):
-            x = nums[i]
+            cur = nums[i]
 
-            if x - previousX == 1:
-                result += 1
-            elif x - previousX == 0:
+            if cur - prev == 1:
+                current += 1
+                prev = cur
+            elif cur - prev == 0:
                 continue
             else:
-                total = max(result,total)
-                result = 1
-
-            previousX = x
-        
-        total = max(result,total)
-        return total
+                result = max(result,current)
+                current = 1
+                prev = cur
+        result = max(result,current)
+        return result
+                
