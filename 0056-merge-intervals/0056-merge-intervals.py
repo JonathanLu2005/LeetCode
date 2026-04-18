e4cab8fd09c37e136d37300adea12914d12af151
JonathanLu2005 <95429781+JonathanLu2005@users.noreply.github.com>
2026-04-18 14:14:34 +0100

    Time: 38 ms (7.51%), Space: 22.3 MB (88.46%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -1,17 +1,22 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        n = len(intervals)
-        i = 0
+        # sort it, and if overlap, merge togeter
+        # a <= d and c <= b, way to overlap
+        # then if overlap, we create them into 1 interval
+        # and keep checking next guy if match, else swap to next guy to use as matcher
         intervals = sorted(intervals)
-        while i < n-1:
-            curX, curY = intervals[i][0], intervals[i][1]
-            nextX, nextY = intervals[i+1][0], intervals[i+1][1]
+        k = len(intervals)
+        i = 0
 
-            if nextX <= curY:
+        while k-1 > 0:
+            curX, curY = intervals[i][0], intervals[i][1]
+            nexX, nexY = intervals[i+1][0], intervals[i+1][1]
+
+            if curX <= nexY and nexX <= curY:
                 intervals.pop(i+1)
-                intervals[i] = [min(curX,nextX), max(curY,nextY)]
-                n -= 1
+                intervals.insert(i,[min(curX,nexX),max(curY,nexY)])
+                intervals.pop(i+1)
             else:
                 i += 1
-
+            k -= 1
         return intervals
