fd1bbc66d8b61cb7813a4b69593fb4afb00d7984
JonathanLu2005 <95429781+JonathanLu2005@users.noreply.github.com>
2026-04-18 18:03:41 +0100

    Time: 305 ms (5.09%), Space: 26.7 MB (22.46%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,35 +1,34 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        # 1 is land, adjacent is island
-        # so given a 1,we bfs
-        # once we covered all 1,thats an island, we icrnement count
-        # wealso need to store the coordinates somewhere
-        #to avoid re exploring an island
-
+        # try to find map of 1
+        # explore the region until no more and store it, need to hold visited to not revisit stuff
+        # then increment
         visited = set()
-        result = 0
         row = len(grid)
-        col = len(grid[0])
+        column = len(grid[0])
+        result = 0
 
         def bfs(x,y):
-            directions = [[0,1],[0,-1],[1,0],[-1,0]]
-            queue = deque([(x,y)])
+            queue = deque([[x,y]])
+            directions = [[0,1],[1,0],[-1,0],[0,-1]]
 
             while queue:
-                x, y = queue.popleft()
+                for i in range(len(queue)):
+                    x, y = queue.popleft()
+                    visited.add((x,y))
 
-                for [dirx,diry] in directions:
-                    curx = x + dirx
-                    cury = y + diry
+                    for [r,c] in directions:
+                        curx = x + r
+                        cury = y + c
 
-                    if curx >= 0 and curx < row and cury >= 0 and cury < col and (curx,cury) not in visited and grid[curx][cury] == "1":
-                        queue.append((curx,cury))
-                        visited.add((curx,cury))
-            return
-            
+                        if curx in range(row) and cury in range(column) and grid[curx][cury] == "1" and (curx,cury) not in visited:
+                            queue.append([curx,cury])
+                            visited.add((curx,cury))
+
         for x in range(row):
-            for y in range(col):
-                if (x,y) not in visited and grid[x][y] == "1":
-                    result += 1
+            for y in range(column):
+                if grid[x][y] == "1" and (x,y) not in visited:
                     bfs(x,y)
+                    result += 1
+
         return result
