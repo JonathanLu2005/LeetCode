8dc09921043c7298442cb73d3fec6269feda1a9b
JonathanLu2005 <95429781+JonathanLu2005@users.noreply.github.com>
2026-04-19 13:47:53 +0100

    Time: 0 ms (100%), Space: 20.9 MB (50.16%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
@@ -4,32 +4,24 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-
-"""
-DFS
-
-"""
-
-
 class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
-        
-        # go through each node, if its not node return true as all good
+        # ahhh so on left must be decreasing, on right must be increasing
+        # moment left is more than parent, return false
+        # else keep going till all good then return true
+        # and whtaever we get return we should add that to final value and see what fina lvalue says
+        # oh yeah if bad, we can pass the value false, andjust and it, then we just return the value
+
         def dfs(node, minVal, maxVal):
             if not node:
                 return True
             
-            # if violate return false
             if not (minVal < node.val < maxVal):
                 return False
 
-            # both needs to be true
-            # going up so node.val and whatnot is their elft or right
             return dfs(node.left, minVal, node.val) and dfs(node.right, node.val, maxVal)
         
         return dfs(root, float("-inf"), float("inf"))
-                
-        
-        
+            
 
-        
+            
