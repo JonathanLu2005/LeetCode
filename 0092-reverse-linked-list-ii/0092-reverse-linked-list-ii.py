5cb8e50cb57184a7afcf445f10dbade2c40176d9
JonathanLu2005 <95429781+JonathanLu2005@users.noreply.github.com>
2026-04-19 11:02:58 +0100

    Time: 0 ms (100%), Space: 19.4 MB (71.7%) - LeetHub

--- 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.py
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.py
@@ -5,31 +5,39 @@
 #         self.next = next
 class Solution:
     def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
-        # just get entire thing, then reverse from left to right
-        # then continue, then amek into linked list
-        array = []
-        reverse = []
+        # reverse the nodes from left to right
+        # uhh so get all the nodes
+        # and we add them in right order to another node
+        # but moment we hit left, then we add it into a stack
+        # until we finish going to right
+        # and once we do that, reverse that,and extend it to the main result
+        # and keep going on further
+        values = []
 
         node = head
-        count = 1
-        while node is not None:
-            if count >= left and count <= right:
-                reverse.append(node.val)
-
-                if count == right:
-                    reverse = reverse[::-1]
-                    array.extend(reverse)
-            else:
-                array.append(node.val)
-            count += 1
+        while node:
+            values.append(node.val)
             node = node.next
+
+        result = []
+        stack = []
+
+        for i in range(len(values)):
+            if (i+1) >= left and (i+1) <= right:
+                stack.append(values[i])
+            else:
+                result.append(values[i])
+
+            if (i+1) == right:
+                stack = stack[::-1]
+                result.extend(stack)
 
         dummyNode = ListNode()
         node = dummyNode
 
-        for x in array:
-            nextNode = ListNode(x)
-            node.next = nextNode
-            node = nextNode
-
+        for x in result:
+            newNode = ListNode(x)
+            node.next = newNode
+            node = newNode
+        
         return dummyNode.next
